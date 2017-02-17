38adcf871fbbf1092435230d2fd946bd9b4a516c
Rik Wout <rikwout@gmail.com>
2017-02-17 16:05:33 +0100

    iOS: refactored RgbVC

--- AllSrcCode/iOS/Ordina_IoT/RgbVC.swift
+++ AllSrcCode/iOS/Ordina_IoT/RgbVC.swift
@@ -32,6 +32,13 @@ class RgbVC: NSObject    {
     private var currentRgbColor: RgbColor = RgbColor.white
     private var rgbSlider: UISlider!
     
+    private let white = UIColor.white
+    private let red = UIColor.red.withAlphaComponent(0.75)
+    private let yellow = UIColor.yellow.withAlphaComponent(0.75)
+    private let green = UIColor.green.withAlphaComponent(0.75)
+    private let aqua = UIColor(red: 153/255, green: 241/255, blue: 250/255, alpha: 1.0)
+    private let blueColor = UIColor.blue.withAlphaComponent(0.75)
+    private let purple = UIColor.purple.withAlphaComponent(0.75)
     
 //Constructor.
     init(frame: CGRect) {
@@ -99,59 +106,61 @@ class RgbVC: NSObject    {
         case 0:
             if currentRgbColor != .white    {
                 currentRgbColor = .white
-                blue.sendMessage(string: "c000\n000\n000")
-                rgbLabel.backgroundColor = UIColor.white
+                blue.sendMessage(string: "c0")
+                setRgbColor(color: white)
             }
         //Red
         case 1..<51:
             if currentRgbColor != .red  {
                 currentRgbColor = .red
-                blue.sendMessage(string: "c255\n000\n000")
-                rgbLabel.backgroundColor = UIColor.red.withAlphaComponent(0.75)
-                rgbSlider.minimumTrackTintColor = UIColor.red.withAlphaComponent(0.75)
+                blue.sendMessage(string: "c1")
+                setRgbColor(color: red)
             }
         //Yellow
         case 51..<101:
             if currentRgbColor != .yellow   {
                 currentRgbColor = .yellow
-                blue.sendMessage(string: "c255\n255\n000")
-                rgbLabel.backgroundColor = UIColor.yellow.withAlphaComponent(0.75)
-                rgbSlider.minimumTrackTintColor = UIColor.yellow.withAlphaComponent(0.75)
+                blue.sendMessage(string: "c2")
+                setRgbColor(color: yellow)
             }
         //Green
         case 101..<151:
             if currentRgbColor != .green    {
                 currentRgbColor = .green
-                blue.sendMessage(string: "c000\n255\n000")
-                rgbLabel.backgroundColor = UIColor.green.withAlphaComponent(0.75)
-                rgbSlider.minimumTrackTintColor = UIColor.green.withAlphaComponent(0.75)
+                blue.sendMessage(string: "c3")
+                setRgbColor(color: green)
             }
         //Aqua
         case 151..<201:
             if currentRgbColor != .aqua {
                 currentRgbColor = .aqua
-                blue.sendMessage(string: "c000\n255\n255")
-                rgbLabel.backgroundColor = UIColor(red: 153/255, green: 241/255, blue: 250/255, alpha: 1.0)
-                rgbSlider.minimumTrackTintColor = UIColor(red: 153/255, green: 241/255, blue: 250/255, alpha: 1.0)
+                blue.sendMessage(string: "c4")
+                setRgbColor(color: aqua)
             }
         //Blue
         case 201..<251:
             if currentRgbColor != .blue {
                 currentRgbColor = .blue
-                blue.sendMessage(string: "c000\n000\n255")
-                rgbLabel.backgroundColor = UIColor.blue.withAlphaComponent(0.75)
-                rgbSlider.minimumTrackTintColor = UIColor.blue.withAlphaComponent(0.75)
+                blue.sendMessage(string: "c5")
+                setRgbColor(color: blueColor)
             }
         //Purple
         case 251..<301:
             if currentRgbColor != .purple   {
                 currentRgbColor = .purple
-                blue.sendMessage(string: "c255\n000\n255")
-                rgbLabel.backgroundColor = UIColor.purple.withAlphaComponent(0.75)
-                rgbSlider.minimumTrackTintColor = UIColor.purple.withAlphaComponent(0.75)
+                blue.sendMessage(string: "c6")
+                setRgbColor(color: purple)
+     
             }
         default:
             break
         }
+    }
+    
+    //Set the rgbLabel's background color en rgb slider's track tint color.
+    private func setRgbColor(color: UIColor) {
+        rgbLabel.backgroundColor = color
+        rgbSlider.minimumTrackTintColor = color
+        
     }
 }
