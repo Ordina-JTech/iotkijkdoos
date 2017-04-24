fd52264b4ba7afb1e35ded9aee960ed1988d1b27
Coen Houtman <coen.houtman@ordina.nl>
2017-04-24 14:28:23 +0200

    Fixed tests where bluetooth is disabled. For some reason the intending statement doesn't work; but the test succeeds without it somehow...

--- AllSrcCode/Android/kijkdoos/app/src/androidTest/java/nl/ordina/kijkdoos/view/BluetoothDisabledTests.java
+++ AllSrcCode/Android/kijkdoos/app/src/androidTest/java/nl/ordina/kijkdoos/view/BluetoothDisabledTests.java
@@ -2,42 +2,35 @@ package nl.ordina.kijkdoos.view;
 
 import android.app.Activity;
 import android.app.Instrumentation;
-import android.bluetooth.BluetoothAdapter;
-import android.content.Intent;
-import android.os.Bundle;
+import android.content.ComponentName;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.intent.Intents;
-import android.support.test.espresso.intent.VerificationMode;
-import android.support.test.espresso.intent.VerificationModes;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.parceler.Parcels;
 
 import javax.inject.Inject;
 
 import nl.ordina.kijkdoos.R;
 import nl.ordina.kijkdoos.ViewBoxApplication;
 import nl.ordina.kijkdoos.bluetooth.AbstractBluetoothService;
-import nl.ordina.kijkdoos.bluetooth.MockedViewBoxRemoteController;
 import nl.ordina.kijkdoos.dagger.MockedApplicationComponent;
-import nl.ordina.kijkdoos.view.control.ControlViewBoxActivity;
 import nl.ordina.kijkdoos.view.search.SearchViewBoxActivity;
 
+import static android.app.Activity.RESULT_CANCELED;
+import static android.bluetooth.BluetoothAdapter.ACTION_REQUEST_ENABLE;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.Intents.intending;
-import static android.support.test.espresso.intent.VerificationModes.*;
+import static android.support.test.espresso.intent.VerificationModes.times;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
 import static org.mockito.Mockito.when;
 
 /**
@@ -48,11 +41,17 @@ import static org.mockito.Mockito.when;
 public class BluetoothDisabledTests {
     @Rule
     public BluetoothDisabledActivityTestRule<SearchViewBoxActivity> searchViewBoxActivityRule =
-            new BluetoothDisabledActivityTestRule<>(SearchViewBoxActivity.class, UserResponse.DONT_ENABLE_BLUETOOTH);
+            new BluetoothDisabledActivityTestRule<SearchViewBoxActivity>(SearchViewBoxActivity.class) {
+                @Override
+                protected void beforeActivityLaunched() {
+                    super.beforeActivityLaunched();
+                    intending(hasAction(ACTION_REQUEST_ENABLE)).respondWith(new Instrumentation.ActivityResult(RESULT_CANCELED, null));
+                }
+            };
 
     @Rule
     public BluetoothDisabledActivityTestRule<BluetoothDisabledActivity> bluetoothDisabledActivityRule =
-            new BluetoothDisabledActivityTestRule<>(BluetoothDisabledActivity.class, UserResponse.ENABLE_BLUETOOTH);
+            new BluetoothDisabledActivityTestRule<>(BluetoothDisabledActivity.class);
 
     @Inject
     AbstractBluetoothService bluetoothService;
@@ -69,26 +68,13 @@ public class BluetoothDisabledTests {
         bluetoothDisabledActivityRule.launchActivity(null);
 
         onView(withId(R.id.enableBluetoothButton)).perform(click());
-        intended(hasComponent(SearchViewBoxActivity.class.getName()));
-    }
-
-    enum UserResponse {
-        ENABLE_BLUETOOTH(Activity.RESULT_OK), DONT_ENABLE_BLUETOOTH(Activity.RESULT_CANCELED);
-
-        private final int userResponse;
-
-        UserResponse(int userResponse) {
-            this.userResponse = userResponse;
-        }
+        intended(hasComponent(new ComponentName(InstrumentationRegistry.getTargetContext(), SearchViewBoxActivity.class)));
     }
 
     private class BluetoothDisabledActivityTestRule<T extends Activity> extends ActivityTestRule<T> {
 
-        private final UserResponse userResponse;
-
-        BluetoothDisabledActivityTestRule(Class<T> activityClass, UserResponse userResponse) {
+        BluetoothDisabledActivityTestRule(Class<T> activityClass) {
             super(activityClass, true, false);
-            this.userResponse = userResponse;
         }
 
         @Override
@@ -103,7 +89,6 @@ public class BluetoothDisabledTests {
             when(bluetoothService.isBluetoothEnabled()).thenReturn(false);
 
             Intents.init();
-            intending(hasAction(BluetoothAdapter.ACTION_REQUEST_ENABLE)).respondWith(new Instrumentation.ActivityResult(userResponse.userResponse, null));
         }
 
         @Override
